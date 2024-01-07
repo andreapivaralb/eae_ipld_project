26d9e1f3594737979bb7287bdc6c5f5b5c796406
Andrea Pivaral <andrea@Andreas-MacBook-Air.local>
2024-01-07 04:32:21 +0100

    Some message

--- pages/3_🌦️_Temperatures_Dashboard.py
+++ pages/3_🌦️_Temperatures_Dashboard.py
@@ -31,7 +31,9 @@ st.divider()
 def load_data():
     data_path = "data/cities_temperatures.csv"
 
-    temps_df = None  # TODO: Ex 3.1: Load the dataset using Pandas, use the data_path variable and set the index column to "show_id"
+    temps_df = pd.read_csv(data_path)  # TODO: Ex 3.1: Load the dataset using Pandas, use the data_path variable and set the index column to "show_id"
+
+    temps_df.head(5) 
 
     if temps_df is not None:
         temps_df["Date"] = pd.to_datetime(temps_df["Date"]).dt.date
@@ -51,25 +53,28 @@ with st.expander("Check the complete dataset:"):
 # TODO: Ex 3.2: Create a new column called `AvgTemperatureCelsius` that contains the temperature in Celsius degrees.
 # temps_df["AvgTemperatureCelsius"] = ...       # uncomment this line to complete it
 
+temps_df["AvgTemperatureCelsius"] = ((temps_df["AvgTemperatureFahrenheit"] - 32) * 5/9)  # TODO: uncomment this line to complete it
+
+temps_df
 
 # ----- Extracting some basic information from the dataset -----
 
 # TODO: Ex 3.3: How many different cities are there? Provide a list of them.
-unique_countries_list = None
+unique_countries_list = temps_df["City"].unique()
 
 # TODO: Ex 3.4: Which are the minimum and maximum dates?
-min_date = None
-max_date = None
+min_date = min(temps_df["Date"]) 
+max_date = max(temps_df["Date"])
 
 # TODO:  Ex 3.5: What are the global minimum and maximum temperatures? Find the city and the date of each of them.
-min_temp = None
-max_temp = None
+min_temp = min(temps_df['AvgTemperatureCelsius'])
+max_temp = max(temps_df['AvgTemperatureCelsius'])
 
-min_temp_city = None
-min_temp_date = None
+min_temp_city = temps_df.loc[temps_df['AvgTemperatureCelsius'].idxmin(), 'City']
+min_temp_date = temps_df.loc[temps_df['AvgTemperatureCelsius'].idxmin(), 'Date']
 
-max_temp_city = None
-max_temp_date = None
+max_temp_city = temps_df.loc[temps_df['AvgTemperatureCelsius'].idxmin(), 'City']
+max_temp_date = temps_df.loc[temps_df['AvgTemperatureCelsius'].idxmin(), 'Date']
 
 
 # ----- Displaying the extracted information metrics -----
